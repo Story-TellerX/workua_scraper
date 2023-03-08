d127d0edb5e53db9d5107b65ef4b45047f1d5ed9
Story-TellerX <ds.churikov85@gmail.com>
2023-03-08 23:04:09 +0200

    1. Main file is created. 2. The structure for scraper run is added

---
+++ main.py
@@ -0,0 +1,24 @@
+def run_the_scraper():
+    # some func
+    pass
+
+
+def check_for_start_scraper() -> bool:
+    i = 0
+    while i < 1:
+        input_user_choice = str(input("Should the scrapping be started?: "))
+        if input_user_choice.lower() == 'n':
+            return False
+        elif input_user_choice.lower() == 'y':
+            i = 1
+            return True
+        else:
+            print("INPUT_NOT_VALID")
+
+
+if __name__ == "__main__":
+    while True:
+        print("MESSAGE_HOW_START_STOP_SCRAPER")
+        if not check_for_start_scraper():
+            break
+        run_the_scraper()
